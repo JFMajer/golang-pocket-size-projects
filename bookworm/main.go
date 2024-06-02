6fb36fccb0c8737c34f5b7e4f485aa3ad585fc11
jakubmajer@live.com <jakubmajer@live.com>
2024-06-02 17:50:15 +0200

    sort recommendations by score

--- bookworm/main.go
+++ bookworm/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"sort"
 )
 
 func main() {
@@ -25,16 +26,34 @@ func main() {
 	// Generate recommendations
 	recommendations := GetRecommendations(bookworms)
 	// Print recommendations
-	for _, rec := range recommendations {
-		fmt.Printf("Recommendations for %s:\n", rec.Name)
-		for book, score := range rec.RecommendedBooks {
-			fmt.Printf(" - %s by %s (Score: %d)\n", book.Title, book.Author, score)
-		}
-	}
+	printReccomendations(recommendations)
 }
 
 func displayBooks(books []Book) {
 	for _, b := range books {
 		fmt.Println("-", b.Title, "by", b.Author)
+	}
+}
+
+func printReccomendations(recommendations []Recommendations) {
+	for _, rec := range recommendations {
+		fmt.Printf("Recommendations for %s:\n", rec.Name)
+
+		sortedBooks := make([]Book, 0, len(rec.RecommendedBooks))
+		for book := range rec.RecommendedBooks {
+			sortedBooks = append(sortedBooks, book)
+		}
+
+		sort.Slice(sortedBooks, func(i, j int) bool {
+			return rec.RecommendedBooks[sortedBooks[i]] > rec.RecommendedBooks[sortedBooks[j]]
+		})
+
+		for _, book := range sortedBooks {
+			score := rec.RecommendedBooks[book]
+			if score > 1 {
+				fmt.Printf(" - %s by %s (Score: %d)\n", book.Title, book.Author, score)
+			}
+		}
+
 	}
 }
